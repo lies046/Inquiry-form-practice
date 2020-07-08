fe79ae8c1d49977a87a76e0330b97b6dd826037a
lies046 <skytree.skysky@gmail.com>
2020-07-08 14:42:46 +0900

    Modified Confirm

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
   function h($str){
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
   }
-
-  var_dump($_SESSION);
+  echo "<pre>";
+  var_dump($_POST);
+  echo "</pre>";
   $pageFlag = 0;
   if(!empty($_POST['btn-confirm'])){
     $pageFlag = 1;
@@ -18,62 +19,109 @@
   }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" contact="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
 
-<?php if($pageFlag === 0) : ?>
-<?php
-  if(!isset($_SESSION['csrfToken'])){
-  $csrfToken = bin2hex(random_bytes(32));
-  $_SESSION['csrfToken'] = $csrfToken;
-  }
-  $token = $_SESSION['csrfToken'];
+    <?php if($pageFlag === 0) : ?>
+      <?php
+        if(!isset($_SESSION['csrfToken'])){
+        $csrfToken = bin2hex(random_bytes(32));
+        $_SESSION['csrfToken'] = $csrfToken;
+        }
+        $token = $_SESSION['csrfToken'];
 
-?>
-  <form method="POST" action="index.php">
-  名前
-    <input type="text" name="your_name" value="<?php echo h($_POST['your_name']) ; ?>">
-    <br>
-  メールアドレス
-    <input type="email" name="email" value="<?php echo h($_POST['email']) ; ?>">
+      ?>
+        <form method="POST" action="index.php">
+          氏名
+          <input type="text" name="your_name" value="<?php echo h($_POST['your_name']) ; ?>">
+          <br>
+          メールアドレス
+          <input type="email" name="email" value="<?php echo h($_POST['email']) ; ?>">
+          <br>
+          ホームページ
+          <input type="url" name="url" value="<?php echo h($_POST['url']) ; ?>">
+          性別
+          <br>
+          <input type="radio" name="gender" value="0">男性
+          <input type="radio" name="gender" value="1">女性
+          <br>
+          年齢
+          <select name="age">
+            <option value="">選択してください</option>
+            <option value="1">~19歳</option>
+            <option value="2">20歳~29歳</option>
+            <option value="3">30歳~39歳</option>
+            <option value="4">40歳~49歳</option>
+            <option value="5">50歳~59歳</option>
+            <option value="6">60歳~69歳</option> 
+          </select>
+          <br>
+          お問い合わせ内容
+          <textarea name="contact" value="<?php echo h($_POST['contact']) ; ?>"></textarea>
+          <br>
 
-    <input type="submit" name="btn-confirm" value="確認する">
-    <input type="hidden" name="csrf" value="<?php echo $token ?>">
-  </form>
-<?php endif; ?>
+          <input type="checkbox" name="caution" value="1">
+          <br>
+          <input type="submit" name="btn-confirm" value="確認する">注意事項にチェックする
+          <input type="hidden" name="csrf" value="<?php echo $token ?>">
+        </form>
+    <?php endif; ?>
 
-<?php if($pageFlag === 1) : ?>
-<?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?> 
-  <form method="POST" action="index.php">
-  名前
-  <?php echo h($_POST['your_name']) ; ?>
-    <br>
-  メールアドレス
-  <?php echo h($_POST['email']) ; ?>
-    <input type="submit" name="back" value="戻る">
-    <input type="submit" name="btn-submit" value="送信する">
-    <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ; ?>">
-    <input type="hidden" name="email" value="<?php echo h($_POST['email']) ; ?>">
-    <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']) ; ?>">
+    <?php if($pageFlag === 1) : ?>
+      <?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?> 
+        <form method="POST" action="index.php">
+          名前
+          <?php echo h($_POST['your_name']) ; ?>
+          <br>
+          メールアドレス
+          <?php echo h($_POST['email']); ?>
+          <br>
+          ホームページ
+          <?php echo h($_POST['url']); ?>
+          <br>
+          性別
+          <?php if($_POST['gender'] === '0'){echo '男性';}
+                if($_POST['gender'] === '1'){echo '女性';}
+          ?>
+          <br>
+          年齢
+          <?php if($_POST['age'] === '1'){echo '~19歳';}
+          elseif($_POST['age'] === '2'){echo '20歳~29歳';}
+          elseif($_POST['age'] === '3'){echo '30歳~39歳';}
+          elseif($_POST['age'] === '4'){echo '40歳~49歳';}
+          elseif($_POST['age'] === '5'){echo '50歳~59歳';}
+          elseif($_POST['age'] === '6'){echo '60歳~69歳';}
+          ?>
+          <br>
+          お問い合わせ内容
+          <?php echo h($_POST['contact']) ; ?>
+          <br>
+            <input type="submit" name="back" value="戻る">
+            <input type="submit" name="btn-submit" value="送信する">
+            <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ; ?>">
+            <input type="hidden" name="email" value="<?php echo h($_POST['email']) ; ?>">
+            <input type="hidden" name="url" value="<?php echo h($_POST['url']) ; ?>">
+            <input type="hidden" name="gender" value="<?php echo h($_POST['gender']) ; ?>">
+            <input type="hidden" name="age" value="<?php echo h($_POST['age']) ; ?>">
+            <input type="hidden" name="contact" value="<?php echo h($_POST['contact']) ; ?>">
 
-  </form>
-  <?php endif; ?>
-<?php endif; ?>
+            <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']) ; ?>">
+        </form>
+      <?php endif; ?>
+    <?php endif; ?>
 
-<?php if($pageFlag === 2) : ?>
-  <?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?> 
-    <?php unset($_SESSION['csrfToken']) ?>
-  完了
-  <?php endif; ?>
-<?php endif; ?>
+    <?php if($pageFlag === 2) : ?>
+      <?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?> 
+        <?php unset($_SESSION['csrfToken']) ?>
+      完了
+      <?php endif; ?>
+    <?php endif; ?>
 
-
-</body>
+  </body>
 </html>
