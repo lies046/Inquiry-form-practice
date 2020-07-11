006c63075cbb07b8de6f77a13de65fb0d043e354
lies046 <skytree.skysky@gmail.com>
2020-07-11 13:57:17 +0900

    Add File

--- file.php
+++ file.php
@@ -2,7 +2,7 @@
 $contactFile = '.contact.dat';
 
 //ファイル丸ごと読み込み
-$fileContents = file_get_contents($contactFile);
+// $fileContents = file_get_contents($contactFile);
 
 // echo $fileContents;
 
@@ -16,12 +16,19 @@ $fileContents = file_get_contents($contactFile);
 
 //配列 file 区切る explode, foreach
 
-$allData = file($contactFile);
+// print_r(file($contactFile));
 
-foreach($allData as $lineData){
-  $lines = explode(',', $lineData);
-  echo $lines[0] . '<br>';
-  echo $lines[1] . '<br>';
-  echo $lines[2] . '<br>';
-  echo $lines[3] . '<br>';
-}
+// foreach($allData as $lineData){
+//   $lines = explode(',', $lineData);
+//   echo $lines[0] . '<br>';
+//   echo $lines[1] . '<br>';
+//   echo $lines[2] . '<br>';
+//   echo $lines[3] . '<br>';
+// }
+
+$contents = fopen($contactFile, 'a+');
+
+$addText = '1行追加' . "\n";
+fwrite($contents, $addText);
+
+fclose($contents);
